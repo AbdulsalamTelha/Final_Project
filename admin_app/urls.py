e3224288d4e086bd59e39e39a07bedeb460ca37e
AkramOthman <akram770455@gmail.com>
2024-12-31 23:12:22 +0300

    navbar

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (access_denied, change_password_view, edit_profile_view,
     instructor_dashboard_view, library_view, login_view, profile_view, student_library_view)
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('',  login_view, name='home'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('change_password/', change_password_view, name='change_password'),
     path('edit_profile/', edit_profile_view, name='edit_profile'),
+    path('', LogoutView.as_view, name='logout'),
 
 ]

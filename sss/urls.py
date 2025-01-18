3265101e70cf9240d1001a0ea1776fa18dfe0b6b
Shidomohd2 <alrasheeds87@gmail.com>
2025-01-18 13:55:03 +0300

    chat app

--- sss/urls.py
+++ sss/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('admin_app.urls')),
     path('admin/', admin.site.urls),
+    path('chat/', include('chat_app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
